da374bdcc587d09ce8adbebc2fbbdf7d77047527
Davoudnsr <davoud.nasrabadi@gmail.com>
2021-01-01 19:25:13 +0330

    setup updated

--- Container/setup.py
+++ Container/setup.py
@@ -1,20 +1,22 @@
 import setuptools
-with open("README.md", "r") as fh:
+
+with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
+
 setuptools.setup(
-    name="ddnMath",
+    name="ddnmath",  # Replace with your own username
     version="0.0.1",
     author="Davoud Nasrabadi",
-    author_email="davoud.nasrabadi@gmail.com",
-    description="a small mathematics library",
+    author_email="davoud.nasrabadi@example.com",
+    description="davoud nasrabadi math package",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/davoudnasrabadi/ddnmath",
     packages=setuptools.find_packages(),
     classifiers=[
-        "Programming Language :: Python ::3",
-        "License :: OSI Approved :: MIT license",
-        "Operating System :: OS Independent"
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
     ],
-    python_requires='>=3.6'
+    python_requires='>=3.6',
 )
